1d52aadc40d68a6b0381c3d6fb30c7f9e67fe61f
vvduth <ducthai060501@gmail.com>
2021-08-29 12:51:38 +0300

    Add project and enable dark mode

--- public/js/resume.js
+++ public/js/resume.js
@@ -10,6 +10,24 @@ function zoomImages(className) {
   });
 }
 
+window.onload = function () {
+  const themeBtn = document.getElementById('toggleBtn');
+  themeBtn.addEventListener('click', function () {
+    // Lấy thuộc tính data-theme
+    const root = document.querySelector(':root');
+    const isLightMode =
+      root.getAttribute('data-theme') === 'dark' ? false : true;
+    // toggle theme mode
+    if (isLightMode) {
+      root.setAttribute('data-theme', 'dark');
+    } else {
+      root.setAttribute('data-theme', 'light');
+    }
+    // thay đổi vị trí của button
+    this.classList.toggle('active');
+  });
+};
+
 (function($) {
   "use strict"; // Start of use strict
 
